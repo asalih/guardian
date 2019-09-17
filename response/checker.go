bd36e3ad3bcaaf6b6bccfdc93c6515caa765fe97
Ahmet Salih <ahmet.salih@windowslive.com>
2019-09-18 00:32:04 +0300

    response checker init

--- response/checker.go
+++ response/checker.go
@@ -1,1 +1,21 @@
+package response
 
+import (
+	"net/http"
+
+	"github.com/asalih/guardian/models"
+)
+
+//Checker Response checker
+type Checker struct {
+	ResponseWriter http.ResponseWriter
+	Request        *http.Request
+
+	responseTransfer *responseTransfer
+	result           chan *models.MatchResult
+}
+
+type responseTransfer struct {
+	BodyBuffer  []byte
+	ContentType string
+}
